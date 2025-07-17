4d91f158a1f700ead49d7b4a4d9d43bf58dcd09a
Jerônimo Santos Rodrigues <santosominorej@gmail.com>
2025-07-17 19:41:52 -0300

    :sparkles: feat: Point adjustments with axios response styles

--- src/app/candidaturas/page.tsx
+++ src/app/candidaturas/page.tsx
@@ -137,7 +137,7 @@ export default function Candidaturas() {
   return (
     <>
       <Header />
-      <main className="flex flex-col min-h-screen p-12 bg-gradient-to-tr from-green-300 via-gray-400 to-gray-500 dark:bg-gradient-to-tr dark:from-gray-500 dark:via-gray-700 dark:to-gray-800 scroll-smooth">
+      <main className="flex flex-col min-h-screen p-48 bg-gradient-to-tr from-green-300 via-gray-400 to-gray-500 dark:bg-gradient-to-tr dark:from-gray-500 dark:via-gray-700 dark:to-gray-800 scroll-smooth">
         <h3 className="text-6xl font-extrabold text-emerald-600 dark:text-blue-200 uppercase hover:underline mb-12">
           Candidaturas
         </h3>
