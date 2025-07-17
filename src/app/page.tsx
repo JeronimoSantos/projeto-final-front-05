b7b8ccf5d126e74983bb9d3e946714b7c21e1965
Jerônimo Santos Rodrigues <santosominorej@gmail.com>
2025-07-17 04:18:18 -0300

    :sparkles: feat: Adjusting and adding more styles and elements to pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,39 @@
 'use client'
 
+import { CarouselDemo } from "@/components/CarouselNoticias";
+import { CarouselDemo1 } from "@/components/CarouselCuriosidades";
 import Navigation from "@/components/Navigation";
 import { ModeToggle }  from '@/components/ThemeSwitcher';
 export default function HomePage() {
   return (
     <>
-      <main className="flex flex-col min-h-screen bg-amber-50 dark:bg-gray-900 p-3.5">
-        <header className="flex flex-row justify-between w-full">
-          <h3 className="text-2xl font-mono text-gray-600">Mercado de Trabalho Inclusivo para PCDs</h3>
+      <main className="flex flex-col min-h-screen bg-gradient-to-tr from-green-300 via-gray-400 to-gray-500 dark:bg-gradient-to-tr dark:from-gray-500 dark:via-gray-700 dark:to-gray-800  p-3.5 scroll-smooth">
+        <header className="flex flex-row justify-between p-1 w-full">
           <ModeToggle />
-
-          <section className="flex flex-col items-center justify-center">
-            <Navigation />
-          </section>
+          <Navigation />
         </header>
 
-        <section>
-          <div className="mt-8 p-6 bg-white-100 dark:bg-gray-400 rounded-lg shadow-md">
-          <h2 className="text-2xl font-semibold">Este é um card de exemplo</h2>
-          <p className="mt-2">Ele também muda de cor!</p>
-        </div>
+        <section className="flex gap-10 justify-around items-center mt-10 p-24 shadow-2xl">
+          <h3 className="text-7xl font-extrabold text-emerald-600 dark:text-blue-200 uppercase text-center">Mercado de Trabalho Inclusivo para PCDs</h3>
+          <img src={"/imgNoticias/logo-pcd.jpeg"} className="w-94 h-94 object-cover rounded-full"/>
+        </section>
+
+        <section className="flex justify-around items-center gap-15 mt-28 p-10">
+          <CarouselDemo />
+
+          <section className="flex flex-col gap-12">
+            <h3 className="text-6xl font-extrabold text-emerald-600 dark:text-blue-200 uppercase hover:underline">Notícias</h3>
+            <p className="text-4xl font-extralight">Notícias atuais e relevantes sobre o mercado de trabalho para Pessoas com Deficiência (PcDs) no Brasil,
+             com links para você acessar e se aprofundar em cada assunto.
+          </p>
+          </section>
+        </section>
+
+        <section className="flex justify-around items-center mt-30 gap-10">
+          <p></p>
+          <CarouselDemo1 />
         </section>
     </main>
-
-    <footer className="flex items-center justify-center">
-      <h3 className="text-gray-500">Desenvolvido com comprometimento com impacto social, acessibilidade e inclusão digital.</h3>
-    </footer>
     </>
   )
 }
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -6,22 +6,22 @@ export default function Navigation() {
         <nav className="flex justify-between">
             <ul className="flex items-center justify-center gap-2 text-2xl">
                 <li>
-                    <Link href="/" target="_self" title="Página Principal" className="text-emerald-400 font-mono">Página Principal</Link>
+                    <Link href="/" target="_self" title="Página Principal" className="text-emerald-400 dark:text-blue-100 font-mono hover:underline">Página Principal</Link>
                 </li>
                 <li>
-                    <Link href="/login" target="_self" title="Login" className="text-emerald-400 font-mono">Login</Link>
+                    <Link href="/login" target="_self" title="Login" className="text-emerald-400 dark:text-blue-100 font-mono hover:underline">Login</Link>
                 </li>
                 <li>
-                    <Link href="/vagas" target="_self" title="Vagas" className="text-emerald-400 font-mono">Vagas</Link>
+                    <Link href="/vagas" target="_self" title="Vagas" className="text-emerald-400 dark:text-blue-100 font-mono hover:underline">Vagas</Link>
                 </li>
                 <li>
-                    <Link href="/candidaturas" target="_self" title="Candidaturas" className="text-emerald-400 font-mono">Candidaturas</Link>
+                    <Link href="/candidaturas" target="_self" title="Candidaturas" className="text-emerald-400 dark:text-blue-100 font-mono hover:underline">Candidaturas</Link>
                 </li>
                 <li>
-                    <Link href="/empresas" target="_self" title="Vagas" className="text-emerald-400 font-mono">Vagas</Link>
+                    <Link href="/empresas" target="_self" title="Vagas" className="text-emerald-400 dark:text-blue-100 font-mono hover:underline">Vagas</Link>
                 </li>
                 <li>
-                    <Link href="/contato" target="_self" title="Contato" className="text-emerald-400 font-mono">Contato</Link>
+                    <Link href="/contato" target="_self" title="Contato" className="text-emerald-400 dark:text-blue-100 font-mono hover:underline">Contato</Link>
                 </li>
             </ul>
         </nav>
